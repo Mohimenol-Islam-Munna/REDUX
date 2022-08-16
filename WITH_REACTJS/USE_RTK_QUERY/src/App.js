19510b873b41fb569a7b1ac2615d4760bb246e8f
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-08-16 17:02:34 +0600

    mutation

--- WITH_REACTJS/USE_RTK_QUERY/src/App.js
+++ WITH_REACTJS/USE_RTK_QUERY/src/App.js
@@ -1,15 +1,44 @@
 import "./App.css";
-import { useGetUserQuery } from "../src/store/features/api/apiSlice";
+import {
+  useGetUserQuery,
+  useCreateUserMutation,
+} from "../src/store/features/api/apiSlice";
 
 const App = () => {
-  const { isLoading, isError, isSuccess, data } = useGetUserQuery();
+  const [createUserMutationHandler, { isLoading }] = useCreateUserMutation();
+  const { isLoading: isUserLoading, data } = useGetUserQuery();
 
-  console.log("post loading ::", isLoading);
-  console.log("post data ::", data);
+  console.log("user create isLoading ::", isLoading);
+  // console.log("user data ::", data);
+
+  // create post handler
+  const createUserHandler = async (data) => {
+    console.log("createPostHandler", data);
+
+    try {
+      const res = await createUserMutationHandler(data).unwrap();
+      console.log("user created successfully :res:", res);
+    } catch (err) {
+      console.log("user does not created");
+    }
+  };
 
   return (
     <div className="App">
       <h2>Awesome RTK Query</h2>
+
+      <div>
+        <button
+          onClick={() =>
+            createUserHandler({
+              name: "Md Mohimenol Islam Munna",
+              job: "React Developer",
+            })
+          }
+        >
+          Create User
+        </button>
+      </div>
     </div>
   );
 };
--- WITH_REACTJS/USE_RTK_QUERY/src/store/features/api/apiSlice.js
+++ WITH_REACTJS/USE_RTK_QUERY/src/store/features/api/apiSlice.js
@@ -8,8 +8,12 @@ const applicationApiSlice = createApi({
     getUser: builder.query({
       query: () => "/api/users",
     }),
+
+    createUser: builder.mutation({
+      query: (data) => ({ url: "/api/users", method: "POST", body: data }),
+    }),
   }),
 });
 
-export const { useGetUserQuery } = applicationApiSlice;
+export const { useGetUserQuery, useCreateUserMutation } = applicationApiSlice;
 export default applicationApiSlice;
