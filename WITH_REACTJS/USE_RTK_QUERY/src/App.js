caa94e23787c9f95192734a0e87218a4f8534342
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-09-25 16:35:19 +0600

    RTK QUERY STRAT

--- WITH_REACTJS/USE_RTK_QUERY/src/App.js
+++ WITH_REACTJS/USE_RTK_QUERY/src/App.js
@@ -1,35 +1,15 @@
 import "./App.css";
-// import {
-//   useGetUserQuery,
-//   useCreateUserMutation,
-// } from "../src/store/features/api/apiSlice";
-
-import { useFetchAllUsersQuery } from "./store/service/applicationApi";
 
 const App = () => {
-  // const [createUserMutationHandler, { isLoading }] = useCreateUserMutation();
-  const { isLoading: isUserLoading, data } = useFetchAllUsersQuery();
-
-  console.log("user isUserLoading ::", isUserLoading);
-  console.log("user data ::", data?.data);
-
-  // create post handler
-  // const createUserHandler = async (data) => {
-  //   console.log("createPostHandler", data);
-
-  //   try {
-  //     const res = await createUserMutationHandler(data).unwrap();
-  //     console.log("user created successfully :res:", res);
-  //   } catch (err) {
-  //     console.log("user does not created");
-  //   }
-  // };
+  const createUserHandler = () => {
+    console.log("create user handler");
+  };
 
   return (
     <div className="App">
       <h2 style={{ textTransform: "uppercase" }}>Awesome RTK Query</h2>
 
-      {/* <div>
+      <div>
         <button
           onClick={() =>
             createUserHandler({
@@ -40,7 +20,7 @@ const App = () => {
         >
           Create User
         </button>
-      </div> */}
+      </div>
     </div>
   );
 };
