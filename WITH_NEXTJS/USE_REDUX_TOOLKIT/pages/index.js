c09729bf317ba8bf28b417ec3aca57f41baccc56
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-10-03 16:23:30 +0600

    next js with redux toolkit

--- WITH_NEXTJS/USE_REDUX_TOOLKIT/pages/index.js
+++ WITH_NEXTJS/USE_REDUX_TOOLKIT/pages/index.js
@@ -1,26 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import storeWrapper from "../store";
 
-export const getStaticProps = storeWrapper.getStaticProps(
-  (store) => async () => {
-    console.log("store in getStaticProps :::", store.getState());
-
-    return {
-      props: {
-        data: "PUST",
-      },
-    };
-  }
-);
-
-const Home = () => {
+export default function Home() {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h2>NEXT JS WITH REDUX TOOlKIT</h2>
+    <div className={styles.container}>
+      <h2>NEXT JS WITH REDUX TOOLKIT</h2>
     </div>
   );
-};
-
-export default Home;
+}
